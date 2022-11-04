4c16f841b49dd1866cd7505713468281cff00464
Trivedh Madala <trivedhmadala@Trivedhs-MacBook-Air.local>
2022-11-05 01:44:11 +0530

    login changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/Login';
+import Register from './components/Register';
+import LoginForm from './components/Loginboot';
+import Logintry from './components/Logintry';
+import RegistrationForm from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Logintry/>
+   
     </div>
   );
 }
