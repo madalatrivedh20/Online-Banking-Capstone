892a91b56e5e96e4965abaf64605f6d242c33da1
Vignesh Srinivasan <vignesh1722@gmail.com>
2022-11-09 23:49:08 +0530

    cleaned the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,36 +6,14 @@ const Footer = () => {
         <MDBFooter color="blue" className="footer">
             <MDBContainer fluid className="text-center text-md-left">
                 <MDBRow>
-                    <MDBCol md="6">
+                    <MDBCol md="12">
                         <h1 className="title">VERTEX BANK GROUP</h1>
                         <h3>
                             The bank where the customer comes first
                         </h3>
                     </MDBCol>
-                    <MDBCol md="6">
-                        <h2 className="title">Important Links</h2>
-                        <ul className="footer-ul">
-                            <li className="list-unstyled">
-                                <a href="/">Home</a>
-                            </li>
-                            <li className="list-unstyled">
-                                <a href="/login">Login</a>
-                            </li>
-                            <li className="list-unstyled">
-                                <a href="/newcheckbook">New chequebook</a>
-                            </li>
-                            <li className="list-unstyled">
-                                <a href="/newfd">New FD</a>
-                            </li>
-                        </ul>
-                    </MDBCol>
                 </MDBRow>
             </MDBContainer>
-            <div className="center footer-copyright text-center py-3">
-                <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-                </MDBContainer>
-            </div>
         </MDBFooter>
     );
 };
